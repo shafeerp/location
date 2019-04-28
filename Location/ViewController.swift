1e74fb16f83f35eea3fce793b853f59b67f05c1d
shafeer <shafeerp@gmail.com>
2019-04-28 09:25:00 +0530

    Initial Commit

---
+++ Location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by Shafeer Puthalan on 28/04/19.
+//  Copyright © 2019 Shafeer Puthalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
